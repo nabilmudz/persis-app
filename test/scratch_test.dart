c318f248d1549125f1f449a7569f8326f3e23d02
nabilmudz <nabilmudzaki306@gmail.com>
2026-05-03 22:27:01 +0700

    local commit

--- test/scratch_test.dart
+++ test/scratch_test.dart
@@ -1,8 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:persis_app/features/BendaharaPJ/data/datasources/transaction_remote_datasources.dart';
-import 'package:persis_app/core/network/api_client.dart';
-import 'package:persis_app/core/storage/secure_storage.dart';
-import 'dart:convert';
+// import 'package:persis_app/core/storage/secure_storage.dart';
 import 'dart:io';
 
 void main() async {
@@ -11,9 +8,9 @@ void main() async {
     HttpOverrides.global = _MyHttpOverrides();
 
     // Since it's a test, we might need a token if the endpoint is protected
-    // If we don't have token, we can just print what we can. 
+    // If we don't have token, we can just print what we can.
     // Wait, we can't easily get token in a pure unit test unless we mock or login.
-    // Instead of unit test, let's write a simple script that logs it from the app or 
+    // Instead of unit test, let's write a simple script that logs it from the app or
     // better, I can just check the backend's response format if the user can run this script.
   });
 }
